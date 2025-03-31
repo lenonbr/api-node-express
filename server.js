0865ef698ae9fd0d9a921f953005e665f8d557f0
lenonbr <lenon.br@hotmail.com>
2025-03-31 10:08:26 -0300

    rota base / criada

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ import http from "http";
 
 const PORT = 3000; 
 
+const rotas = {
+    "/": "Curso de Node.js"
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Curso de Node.js");
+    res.end(rotas[req.url]);
 });
 
 server.listen(3000, () => {
