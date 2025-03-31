c38409d5c16539e8a3d6d6125181ce00ed3db912
lenonbr <lenon.br@hotmail.com>
2025-03-31 09:55:20 -0300

    porta 3000 criada

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import http from "http"; 
 
+const PORT = 3000; 
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("Curso de Node.js");
 });
 
 server.listen(3000, () => {
-    console.log("servidor escutando!");
+    console.log(`servidor escutando na porta ${PORT}`);
 });
