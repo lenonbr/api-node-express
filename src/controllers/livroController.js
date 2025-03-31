75cf033133ac7303a4387175e2288d70de12631d
lenonbr <lenon.br@hotmail.com>
2025-03-31 17:20:51 -0300

    refatorando e atualizando rotas

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,6 +1,20 @@
-import livro from "../models/Livro..js";
+import livro from "../models/Livro.js";
 
 class LivroController {
+
+    static async listarLivros (req, res) {
+        const listaLivros = await livro.find({});
+        res.status(200).json(listaLivros);
+    }
+
+    static async cadastrarLivro (req, res) {
+        try {
+            const novoLivro = await livro.create(req.body);
+            res.status(201).json({message: "Criado com sucesso", livro: novoLivro});
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - falha ao cadastrar livro.`});
+        }
+    }
 
 };
 
