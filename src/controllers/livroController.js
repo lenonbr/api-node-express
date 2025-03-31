d8a0434ae9adecce33450c36d606224e7c6e5fff
lenonbr <lenon.br@hotmail.com>
2025-03-31 17:56:18 -0300

    CRUD feito com sucesso com rotas atualizadas pelo controller

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,9 +3,25 @@ import livro from "../models/Livro.js";
 class LivroController {
 
     static async listarLivros (req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
-    }
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } 
+        catch (erro) {
+            res.status(500).json({message: `${erro.message} - falha na requisição`});
+        }
+    };
+
+    static async listarLivroPorId (req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } 
+        catch (erro) {
+            res.status(500).json({message: `${erro.message} - falha na requisição do livro`});
+        }
+    };
 
     static async cadastrarLivro (req, res) {
         try {
@@ -14,7 +30,27 @@ class LivroController {
         } catch (erro) {
             res.status(500).json({message: `${erro.message} - falha ao cadastrar livro.`});
         }
-    }
+    };
+    static async atualizarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message: "livro atualizado"});
+        } 
+        catch (erro) {
+            res.status(500).json({message: `${erro.message} - falha na atualização do livro`});
+        }
+    };
+    static async exluirLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({message: "livro exluído com sucesso"});
+        } 
+        catch (erro) {
+            res.status(500).json({message: `${erro.message} - falha na exlusão do livro`});
+        }
+    };
 
 };
 
